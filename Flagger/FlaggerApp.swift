b9d488ca907eac69cb5e5098b5362cc2839d5c71
Ege Cavusoglu <egecavusoglu99@gmail.com>
2020-11-10 19:56:50 +0300

    Initial Commit

---
+++ Flagger/FlaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaggerApp.swift
+//  Flagger
+//
+//  Created by Ege Çavuşoğlu on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FlaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
